00c8316ddc2991fd2c530a8d8d601c3bf8ea1f60
Godfrey Chinedu Victor <godfrey97@yahoo.com>
2019-11-22 14:46:50 +0100

    Updated Factorial method

--- Factorial.java
+++ Factorial.java
@@ -1,4 +1,21 @@
 
 public class Factorial {
 	//todo: implement a recursive functions whicht gives you the factorial value of a given number
+	
+	public static int factorial(int n) {
+		if (n==0) {
+			return 0;
+		}
+		else if (n==1) {
+			return 1;
+		}
+		
+		else {
+			return n * factorial(n-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.print(factorial(5));
+	}
 }
